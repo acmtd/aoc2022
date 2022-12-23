36030be2b98bc552b61fee572c788a917202f2c1
Andy Cross <across@dextersystems.com>
2022-12-23 15:08:43 -0500

    day 11 part 2 with a bit of help from hints on AOC subreddit

--- src/Day11.kt
+++ src/Day11.kt
@@ -1,13 +1,20 @@
-data class Item(var worryLevel: Int) {
-    fun operate(op: Operation, test: Int, worryEases: Boolean): Boolean {
+import java.math.BigInteger
+
+data class Item(var worryLevel: BigInteger) {
+    fun operate(op: Operation, test: BigInteger, worryEases: Boolean, modulo: BigInteger): Boolean {
         worryLevel = op.applyTo(worryLevel)
-        if (worryEases) worryLevel /= 3
-        return (worryLevel % test == 0)
+
+        if (worryEases) {
+            worryLevel = worryLevel.divide(BigInteger.valueOf(3))
+        } else {
+            worryLevel %= modulo
+        }
+        return (worryLevel.mod(test).equals(BigInteger.ZERO))
     }
 }
 
-data class Operation(val op: String, val amount: Int?) {
-    fun applyTo(worryLevel: Int): Int {
+data class Operation(val op: String, val amount: BigInteger?) {
+    fun applyTo(worryLevel: BigInteger): BigInteger {
         if (op == "*") return worryLevel * amount!!
         if (op == "+") return worryLevel + amount!!
 
@@ -19,7 +26,7 @@ data class Operation(val op: String, val amount: Int?) {
             val (op, amount) = str.split(" ")
 
             if (amount == "old") return Operation("sq", null)
-            return Operation(op, amount.toInt())
+            return Operation(op, amount.toBigInteger())
         }
     }
 }
@@ -27,25 +34,25 @@ data class Operation(val op: String, val amount: Int?) {
 data class Monkey(
     val items: MutableList<Item>,
     var operation: Operation,
-    var divisibleTest: Int,
+    var divisibleTest: BigInteger,
     var throwTo: List<Int>
 ) {
-    var inspectedItems = 0
+    var inspectedItems: Long = 0
 
-    fun operate(item: Item, worryEases: Boolean): Boolean {
+    fun operate(item: Item, worryEases: Boolean, modulo: BigInteger): Boolean {
         inspectedItems++
-        return item.operate(operation, divisibleTest, worryEases)
+        return item.operate(operation, divisibleTest, worryEases, modulo)
     }
 
     companion object {
         fun of(lines: List<String>): Monkey {
             val startItems = lines[1].substringAfter(": ")
                 .split(",")
-                .map { Item(it.trim().toInt()) }
+                .map { Item(it.trim().toBigInteger()) }
                 .toMutableList()
 
             val operation = Operation.of(lines[2].substringAfter("old "))
-            val divisibleTest = lines[3].substringAfter("divisible by ").toInt()
+            val divisibleTest = lines[3].substringAfter("divisible by ").toBigInteger()
             val throwTo = setOf(4, 5)
                 .map { lines[it].substringAfter("monkey ").toInt() }
 
@@ -55,10 +62,10 @@ data class Monkey(
 }
 
 fun main() {
-    fun processRound(monkeys: List<Monkey>, worryEases: Boolean) {
+    fun processRound(monkeys: List<Monkey>, worryEases: Boolean, modulo: BigInteger) {
         monkeys.forEach { monkey ->
             monkey.items.forEach { item ->
-                if (monkey.operate(item, worryEases)) {
+                if (monkey.operate(item, worryEases, modulo)) {
                     monkeys[monkey.throwTo[0]].items.add(item)
                 } else {
                     monkeys[monkey.throwTo[1]].items.add(item)
@@ -69,22 +76,42 @@ fun main() {
         }
     }
 
-    fun part1(input: String): Int {
+    fun part1(input: String): Long {
         val monkeys = input
             .split("\n\n")
             .map { Monkey.of(it.lines()) }
 
+        val modulo = monkeys.map { it.divisibleTest }
+            .reduce { a, b -> a * b }
+
         repeat(20) {
-            processRound(monkeys, true)
+            processRound(monkeys, true, modulo)
         }
 
-        return monkeys.map { it.inspectedItems }.sortedDescending().take(2).reduce { a, b -> a * b }
+        return monkeys.map { it.inspectedItems }.sortedDescending().take(2).reduce { a, b -> a * b }.toLong()
+    }
+
+    fun part2(input: String): Long {
+        val monkeys = input
+            .split("\n\n")
+            .map { Monkey.of(it.lines()) }
+
+        val modulo = monkeys.map { it.divisibleTest }
+            .reduce { a, b -> a * b }
+
+        repeat(10000) {
+            processRound(monkeys, false, modulo)
+        }
+
+        return monkeys.map { it.inspectedItems }.sortedDescending().take(2).reduce { a, b -> a * b }.toLong()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readText("Day11_test")
-    check(part1(testInput) == 10605)
+    check(part1(testInput) == 10605.toLong())
+    check(part2(testInput) == 2713310158)
 
     val input = readText("Day11")
     part1(input).println() // 110888
+    part2(input).println() // 25590400731
 }

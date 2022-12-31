2189a4bc413bd2fab7a6580c81ec664f857f5476
Andy Cross <andy@acmtd.com>
2022-12-31 14:51:21 -0500

    day 16 part 2 solution

--- src/Day16.kt
+++ src/Day16.kt
@@ -1,35 +1,47 @@
-data class Valve(val name: String, val flowRate: Int, val connectedTo: List<String>) {
-    override fun toString(): String {
-        return "$name [$flowRate]"
-    }
-}
-
-data class MinuteScore(val minutesRemaining: Int, val score: Int) {
-    fun afterOpen(valveOpened: Valve, travelTime: Int): MinuteScore {
-        val scoreMins = minutesRemaining - travelTime
-
-        return MinuteScore(scoreMins, score + (scoreMins * valveOpened.flowRate))
-    }
-
-    fun inferiorTo(other: MinuteScore) = score < other.score
-}
-
-data class ValveState(val current: Valve, val closed: Set<Valve>) {
-    fun open(valve: Valve) = ValveState(valve, closed.minus(valve))
-}
-
-data class State(val score: MinuteScore, val valveState: ValveState) {
-    fun moveAndOpen(target: Valve, routing: List<Pair<Valve, Map<Valve, Valve?>>>) =
-        State(score.afterOpen(target, routeTo(target, routing).size), valveState.open(target))
-
-    fun possibleNextMoves(routing: List<Pair<Valve, Map<Valve, Valve?>>>) =
-        valveState.closed.filter { routeTo(it, routing).size <= score.minutesRemaining }
-
-    private fun routeTo(end: Valve, routing: List<Pair<Valve, Map<Valve, Valve?>>>) =
-        shortestPath(routing.first { it.first == valveState.current }.second, valveState.current, end)
-}
+typealias Routing<T, U> = List<Pair<T, Map<T, U>>>
 
 fun main() {
+    data class Valve(val name: String, val flowRate: Int, val connectedTo: List<String>) {
+        override fun toString() = "$name [$flowRate]"
+    }
+
+    data class UniquePosition(val playerPositions: Set<Valve>, val closedValves: Set<Valve>)
+
+    data class Player(val current: Valve, val remaining: Int)
+
+    data class Move(val player: Player, val target: Valve)
+
+    data class State(val score: Int, val players: List<Player>, val closedValves: Set<Valve>) {
+        fun makeMoves(moves: List<Move>, routing: Routing<Valve, Valve?>): State {
+            var state = this
+
+            moves.forEach { move -> state = state.makeSingleMove(move, routing) }
+
+            return state
+        }
+
+        private fun makeSingleMove(move: Move, routing: Routing<Valve, Valve?>): State {
+            val timeTaken = routeTo(move.player.current, move.target, routing).size
+
+            val newPlayer = Player(move.target, move.player.remaining - timeTaken)
+            val moveScore = newPlayer.remaining * move.target.flowRate
+
+            return State(
+                this.score + moveScore,
+                players.minus(move.player).plus(newPlayer),
+                closedValves.minus(move.target)
+            )
+        }
+
+        fun possibleMovesForPlayer(player: Player, routing: Routing<Valve, Valve?>) =
+            closedValves.filter { routeTo(player.current, it, routing).size <= player.remaining }
+                .sortedByDescending { it.flowRate }
+                .map { Move(player, it) }
+
+        private fun routeTo(from: Valve, to: Valve, routing: Routing<Valve, Valve?>) =
+            shortestPath(routing.first { it.first == from }.second, from, to)
+    }
+
     fun parse(input: List<String>): List<Valve> {
         return input.map {
             val words = it.split(" ", ",").filter { x -> x.isNotEmpty() }
@@ -50,52 +62,70 @@ fun main() {
         return Pair(weights, edges)
     }
 
-    fun configureRouting(allValves: List<Valve>): List<Pair<Valve, Map<Valve, Valve?>>> {
+    fun configureRouting(allValves: List<Valve>): Routing<Valve, Valve?> {
         val (weights, edges) = configureRoutingGraph(allValves)
         return allValves.map { it to dijkstra(Graph(allValves.toSet(), edges, weights), it) }
     }
 
-    fun part1(input: List<String>): Int {
+    fun possibleMoves(state: State, routing: Routing<Valve, Valve?>): List<List<Move>> {
+        val allPossibleMoves =
+            state.players.map { state.possibleMovesForPlayer(it, routing) }.filter { it.isNotEmpty() }
+
+        if (allPossibleMoves.isEmpty()) return emptyList()
+
+        if (allPossibleMoves.size == 1) {
+            return allPossibleMoves.first().map { listOf(it) }
+        } else if (allPossibleMoves.size == 2) {
+            return allPossibleMoves[0].cartesianProduct(allPossibleMoves[1]) { a, b -> listOf(a, b) }
+                .filter { it[0].target != it[1].target }
+        }
+
+        error("Can't handle more than 2 players")
+    }
+
+    fun runSimulation(minutes: Int, players: Int, input: List<String>): Int {
+        if (players > 2) error("More than 2 players not supported")
+
         val allValves = parse(input)
         val routing = configureRouting(allValves)
 
         val valvesWithFlow = allValves.filter { it.flowRate > 0 }.toSet()
         val startPosition = allValves.first()
 
-        val initialState = State(MinuteScore(30, 0), ValveState(startPosition, valvesWithFlow))
+        val initialState = State(0, List(players) { Player(startPosition, minutes) }, valvesWithFlow)
 
         // set up initial moves from the start position
-        val queue = ArrayDeque<Pair<State, Valve>>()
-
-        queue.addAll(initialState.possibleNextMoves(routing)
-            .sortedByDescending { it.flowRate }
-            .map { initialState to it })
+        val queue = ArrayDeque<Pair<State, List<Move>>>()
+        val initialMoves = possibleMoves(initialState, routing).map { initialState to it }
+        queue.addAll(initialMoves)
 
         var maxScore = 0
         var maxScoreState = initialState
 
-        val scoreMap = hashMapOf<ValveState, MinuteScore>()
+        val scoreMap = hashMapOf<UniquePosition, Int>()
 
         while (queue.isNotEmpty()) {
-            val (state, target) = queue.removeFirst()
+            val (state, moves) = queue.removeFirst()
 
-            val newState = state.moveAndOpen(target, routing)
+            val newState = state.makeMoves(moves, routing)
 
-            val priorScore = scoreMap.getOrDefault(newState.valveState, newState.score)
+            val position = UniquePosition(state.players.map { it.current }.toSet(), newState.closedValves)
+
+            val priorScore = scoreMap.getOrDefault(position, newState.score)
 
             // ensure that this is not a less optimal way to reach a state we already know about
-            if (!newState.score.inferiorTo(priorScore)) {
-                scoreMap[newState.valveState] = newState.score
+            if (newState.score >= priorScore) {
+                scoreMap[position] = newState.score
 
                 // check for new high score
-                if (newState.score.score > maxScore) {
-                    maxScore = newState.score.score
+                if (newState.score > maxScore) {
+                    maxScore = newState.score
                     maxScoreState = newState
                 }
 
-                queue.addAll(newState.possibleNextMoves(routing)
-                    .sortedByDescending { it.flowRate }
-                    .map { newState to it })
+                val nextMoves = possibleMoves(newState, routing).map { newState to it }
+
+                queue.addAll(nextMoves.filter { it.second.isNotEmpty() })
             }
         }
 
@@ -103,10 +133,19 @@ fun main() {
         return maxScore
     }
 
+    fun part1(input: List<String>): Int {
+        return runSimulation(30, 1, input)
+    }
+
+    fun part2(input: List<String>): Int {
+        return runSimulation(26, 2, input)
+    }
+
     val testInput = readInput("Day16_test")
     check(part1(testInput) == 1651)
-//    check(part2(testInput) == 1707)
+    check(part2(testInput) == 1707)
 
     val input = readInput("Day16")
     part1(input).println() // 1775
+    part2(input).println() // 2351
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -105,3 +105,11 @@ fun <T> shortestPath(shortestPathTree: Map<T, T?>, start: T, end: T): List<T> {
 
     return pathTo(start, end)
 }
+
+fun <T, S> Collection<T>.cartesianProduct(other: Iterable<S>): List<Pair<T, S>> {
+    return cartesianProduct(other) { first, second -> first to second }
+}
+
+fun <T, S, V> Collection<T>.cartesianProduct(other: Iterable<S>, transformer: (first: T, second: S) -> V): List<V> {
+    return this.flatMap { first -> other.map { second -> transformer.invoke(first, second) } }
+}
